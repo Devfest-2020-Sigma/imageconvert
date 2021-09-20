06d4580298e6a7bf9e030a8222bb3f558ae6ed8b
Rabouin Geoffroy <geoffroy.r@bouin.es>
2021-09-20 13:58:01 +0200

    Update setup_pi_photo.sh

--- setup_pi_photo.sh
+++ setup_pi_photo.sh
@@ -152,6 +152,18 @@ done
 EOF
 sudo chmod +x /usr/local/bin/LaunchWhenReady
 
+cat > /usr/share/dispsetup.sh << EOF
+#!/bin/sh
+if ! grep -q 'Raspberry Pi' /proc/device-tree/model || (grep -q okay /proc/device-tree/soc/v3d@7ec00000/status 2> /dev/null || grep -q okay /proc/device-tree/soc/firmwarekms@7e600000/status 2> /dev/null || grep -q okay /proc/device-tree/v3dbus/v3d@7ec04000/status 2> /dev/null) ; then
+if xrandr --output HDMI-1 --primary --mode 1600x900 --rate 60.00 --pos 0x0 --rotate left --dryrun ; then 
+xrandr --output HDMI-1 --primary --mode 1600x900 --rate 60.00 --pos 0x0 --rotate left
+fi
+fi
+if [ -e /usr/share/tssetup.sh ] ; then
+. /usr/share/tssetup.sh
+fi
+EOF
+
 # A faire automatiquement :
 # dans /usr/share/X11/xorg.conf.d/40-libinput.conf
 #   Identifier "libinput touchscreen catchall"
