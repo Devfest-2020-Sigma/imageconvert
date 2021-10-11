74c33279116460d6597ec5695e5e970195ba922f
Rabouin Geoffroy <geoffroy.r@bouin.es>
2021-10-11 14:27:43 +0200

    sleep is better than nothing

--- reset-firmware.sh
+++ reset-firmware.sh
@@ -6,13 +6,19 @@ IP=$1
 unset http_proxy
 
 curl "http://${IP}:8080/api/v1/machine/sendGcode" -H 'Content-Type: application/json' --data-raw '{"commands":"$21 = 0"}'
+sleep 1
 curl "http://${IP}:8080/api/v1/machine/sendGcode" -H 'Content-Type: application/json' --data-raw '{"commands":"$100 = 77.000"}'
+sleep 1
 curl "http://${IP}:8080/api/v1/machine/sendGcode" -H 'Content-Type: application/json' --data-raw '{"commands":"$101 = 77.000"}'
+sleep 1
 curl "http://${IP}:8080/api/v1/machine/sendGcode" -H 'Content-Type: application/json' --data-raw '{"commands":"$110 = 20000.000"}'
+sleep 1
 curl "http://${IP}:8080/api/v1/machine/sendGcode" -H 'Content-Type: application/json' --data-raw '{"commands":"$111 = 20000.000"}'
+sleep 1
 curl "http://${IP}:8080/api/v1/machine/sendGcode" -H 'Content-Type: application/json' --data-raw '{"commands":"$120 = 8000.000"}'
+sleep 1
 curl "http://${IP}:8080/api/v1/machine/sendGcode" -H 'Content-Type: application/json' --data-raw '{"commands":"$121 = 8000.000"}'
+sleep 1
 curl "http://${IP}:8080/api/v1/machine/sendGcode" -H 'Content-Type: application/json' --data-raw '{"commands":"$130 = 220.000"}'
+sleep 1
 curl "http://${IP}:8080/api/v1/machine/sendGcode" -H 'Content-Type: application/json' --data-raw '{"commands":"$131 = 220.000"}'
-
-
